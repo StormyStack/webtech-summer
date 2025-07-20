fc04ebf2e4c7bcb6a80940151f6438f391ecd8e7
Sudipto Mondal <134280690+StormyStack@users.noreply.github.com>
2025-07-20 10:34:03 +0600

    Add files via upload
    Lab Task _1

---
+++ Form.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<title>
+    This is a from
+</title>
+<body>
+    <center>
+        <h1 style="color: blue;">AIUB</h1>
+        <h2 style="color: blue;">Registration Form</h2>
+    </center>
+
+    <h3>Complete the Registration </h3>
+<table>
+<tr><td>Full Name:</td><td><input type="text"></td></tr>
+<tr><td>Email:</td><td><input type="email"></td></tr>
+<tr><td>Password:</td><td><input type="password"></td>
+<tr>
+    <td>Gender:</td>
+    <td><input type="radio" name="des"> Male
+    <input type="radio" name="des">Female
+    <input type="radio" name="des">Other</td>
+</tr>
+<tr>
+    <td>Language Known:</td>
+    <td><input type="radio" name="des"> Engilish
+    <input type="radio" name="des">Bengali
+    <input type="radio" name="des">Hindi</td>
+</tr>
+<tr><td>Country:</td>
+<td>
+    <select name="country" id="country">
+        <option value="">-----Select-----</option>
+        <option value="BD">Bangladesh</option>
+        <option value="IN">India</option>
+        <option value="CN">China</option>
+        <option value="USA">United States of America</option>
+        <option value="GB">United Kingdom</option>
+    </select>
+</td>
+</tr>
+<tr>
+    <td>Date Of Birth:</td> <td><input type="Date"></td>
+</tr>
+<tr><td>Upload Photo:</td><td><input type="file" name="Photo"></td></tr>
+<tr><td>Comments:</td><td><textarea></textarea></td></tr>
+<tr><td>   </td><td><input type="submit" value="Register"></td></tr>
+</body>
+</table>
+</html>
+
+
+
+
+
+
+
+</html>
